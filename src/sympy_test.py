d3515d6c72b7ab093917250b388e7b136735d07e
psaunderualberta <psaunder@ualberta.ca>
2025-09-12 11:16:46 -0600

    Added loss function to t5

--- src/sympy_test.py
+++ src/sympy_test.py
@@ -7,6 +7,11 @@ g_prime = Function("g'")
 class g(Function):
     def fdiff(self, argindex=1):
         return g_prime(self.args[0])
+    
+
+class l(Function):
+    def fdiff(self, argindex=1):
+        return g(self.args[0])
 
 
 def main():
@@ -23,11 +28,12 @@ def main():
     t5 = t4 - alpha * (g(t4) + n4 * s4)
     for s in [s4, s3, s2, s1, s0]:
         print(r"\frac{d(t5)}{d(" + str(s) + r")} &= ", end="")
-        print(latex(factor(t5.diff(s)
+        print(latex(factor(l(t5).diff(s)
                 .subs(w0 - alpha * (g(w0) + n0 * s0), w1)
                 .subs(w1 - alpha * (g(w1) + n1 * s1), w2)
                 .subs(w2 - alpha * (g(w2) + n2 * s2), w3)
                 .subs(w3 - alpha * (g(w3) + n3 * s3), w4)
+                .subs(w4 - alpha * (g(w4) + n4 * s4), symbols('w5'))
                 )), end=r" \\" + "\n")
     
     exit()
