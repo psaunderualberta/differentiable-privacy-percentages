884efba527377bd6073a8a8ebd78e826a110eb73
psaunderualberta <psaunder@ualberta.ca>
2026-02-19 13:10:13 -0700

    Added plotting_interval=10 to sweep starter

--- cc/multi-sweep-starter.sh
+++ cc/multi-sweep-starter.sh
@@ -9,7 +9,7 @@ for i in {0..1}; do
             time uv run sweep.py --wandb_conf.project="Testing Mu-gdp" --wandb-conf.entity psaunder --wandb-conf.mode online \
                 --sweep.total_timesteps 2000  --sweep.policy.batch_size 12 --sweep.env.eps $eps --sweep.env.delta 1e-6 --sweep.env.network_type cnn \
                 --sweep.env.max_steps_in_episode 3000 --sweep.env.optimizer sgd --sweep.policy.schedule_type=$SCHEDULE_TYPE \
-                --sweep.policy.momentum.value=$MOMENTUM \
+                --sweep.policy.momentum.value=$MOMENTUM --sweep.plotting_interval=10\
                 --sweep.with-baselines --sweep.dataset "$dataset" --sweep.name "$dataset, e=$eps, T=3000, $SCHEDULE_TYPE, M=$MOMENTUM"
         done
     done
